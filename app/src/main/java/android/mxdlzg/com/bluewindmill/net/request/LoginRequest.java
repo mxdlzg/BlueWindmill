a4cc238ae30f46e1a9b0e11289a77fc85f7a2385
mxdlzg <mxdlzg@163.com>
2018-01-02 21:47:07 +0800

    添加第二课堂内容的网络请求 baseRequest暂时不可用 添加jsoup的源码lib

--- app/src/main/java/android/mxdlzg/com/bluewindmill/net/request/LoginRequest.java
+++ app/src/main/java/android/mxdlzg/com/bluewindmill/net/request/LoginRequest.java
@@ -2,7 +2,7 @@ package android.mxdlzg.com.bluewindmill.net.request;
 
 import android.content.Context;
 import android.mxdlzg.com.bluewindmill.R;
-import android.mxdlzg.com.bluewindmill.model.entity.config.Config;
+import android.mxdlzg.com.bluewindmill.model.config.Config;
 import android.mxdlzg.com.bluewindmill.model.local.ManageSetting;
 import android.mxdlzg.com.bluewindmill.net.callback.CommonCallback;
 import android.widget.Toast;
@@ -16,12 +16,13 @@ import com.lzy.okgo.model.Response;
  */
 
 public class LoginRequest {
-    public static int loginStatus = Config.NOT_LOGIN;
+    public static int emsLoginStatus = Config.NOT_LOGIN;
+    public static int scLoginStatus = Config.NOT_LOGIN;
 
     /**
      * Read Params from config(user,password)
      */
-    public void loginEMS(final Context context, final CommonCallback callback) {
+    public void loginEMS(final Context context, final CommonCallback<String> callback) {
         OkGo.<String>get(Config.EMS_LOGIN_URL)
                 .tag(this)
                 .params("loginName", ManageSetting.getStringSetting(context, Config.USER_NAME))
@@ -34,21 +35,46 @@ public class LoginRequest {
                     @Override
                     public void onSuccess(Response<String> response) {
                         if (!response.body().contains(context.getString(R.string.loginCheckEMS))){
-                            loginStatus = Config.NOT_LOGIN;
+                            emsLoginStatus = Config.NOT_LOGIN;
                             callback.onFail(context.getString(R.string.loginFailEMS));
                         }else {
-                            loginStatus = Config.LOGIN;
+                            emsLoginStatus = Config.LOGIN;
                             callback.onSuccess(response.message());
                         }
                     }
 
                     @Override
                     public void onError(Response<String> response) {
-                        loginStatus = Config.NOT_LOGIN;
+                        emsLoginStatus = Config.NOT_LOGIN;
                         Toast.makeText(context, Config.codeConvertor(response.code()), Toast.LENGTH_SHORT).show();
                         if (callback!=null){
                             callback.onFail(Config.codeConvertor(response.code()));
                         }
+                    }
+                });
+    }
+
+    /**
+     * login into SC
+     * @param context context
+     * @param callback callback
+     */
+    public static void loginSC(final Context context,final CommonCallback<String> callback){
+        OkGo.<String>head(Config.SC_LOGIN_URL)
+                .tag(context)
+                .params("j_username",Config.USER_NAME)
+                .params("returnUrl","")
+                .execute(new StringCallback() {
+                    @Override
+                    public void onSuccess(Response<String> response) {
+                        scLoginStatus = Config.LOGIN;
+                        callback.onSuccess(Config.codeConvertor(response.code()));
+                    }
+
+                    @Override
+                    public void onError(Response<String> response) {
+                        scLoginStatus = Config.NOT_LOGIN;
+                        callback.onError(Config.codeConvertor(response.code()));
                     }
                 });
     }
