83ba442e5c35b23a8696e49f12bb8c293c49d6da
mxdlzg <mxdlzg@163.com>
2018-01-02 21:45:33 +0800

    添加对第二课堂内容处理的process模块，对base模块做小更新

--- app/src/main/java/android/mxdlzg/com/bluewindmill/model/process/BaseProcess.java
+++ app/src/main/java/android/mxdlzg/com/bluewindmill/model/process/BaseProcess.java
@@ -35,4 +35,12 @@ public class BaseProcess {
         }
         return dataTable;
     }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
