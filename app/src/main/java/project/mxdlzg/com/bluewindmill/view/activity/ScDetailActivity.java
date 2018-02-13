994cfffb61c124d16337bc87cca5ee7298a4433b
mxdlzg <mxdlzg@163.com>
2018-02-13 11:40:48 +0800

    调整ui完毕

--- app/src/main/java/project/mxdlzg/com/bluewindmill/view/activity/ScDetailActivity.java
+++ app/src/main/java/project/mxdlzg/com/bluewindmill/view/activity/ScDetailActivity.java
@@ -1,9 +1,20 @@
 package project.mxdlzg.com.bluewindmill.view.activity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.transition.ChangeBounds;
+import android.transition.ChangeClipBounds;
+import android.transition.ChangeImageTransform;
+import android.transition.ChangeTransform;
+import android.transition.Explode;
+import android.transition.Fade;
+import android.transition.TransitionManager;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Toast;
 
 import br.com.simplepass.loading_button_lib.customViews.CircularProgressButton;
@@ -27,11 +38,18 @@ public class ScDetailActivity extends AppCompatActivity {
         setContentView(R.layout.layout_sc_activity_detail);
         ButterKnife.bind(this);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setEnterTransition(new Explode());
+            getWindow().setExitTransition(null);
+        }
+        final Animation fadein = AnimationUtils.loadAnimation(this,R.anim.view_fade_in);
+
         findViewById(R.id.sc_dtl_progressBar).postDelayed(new Runnable() {
             @Override
             public void run() {
                 findViewById(R.id.sc_dtl_progressBar).setVisibility(View.GONE);
                 findViewById(R.id.relativeLayout2).setVisibility(View.VISIBLE);
+                findViewById(R.id.relativeLayout2).startAnimation(fadein);
             }
         },1000);
 
